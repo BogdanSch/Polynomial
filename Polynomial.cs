b1905c8494030d662f4dab68cffa7da7ab31a22f
Bogdan <68709795+BogdanSch@users.noreply.github.com>
2022-02-19 14:49:00 +0200

    Add files via upload

---
+++ Polynomial.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polynomial
+{
+    class Polynomial
+    {
+        public Polynomial(char variable, List<double> coeficients, List<double> degree)
+        {
+            Variable = variable;
+            Coeficients = coeficients;
+            Degree = degree;
+        }
+
+        public Polynomial() { }
+
+        char Variable { get; set; } = 'x';
+        List<double> Coeficients { get; set; } = new List<double>();
+        List<double> Degree { get; set; } = new List<double>();
+
+        public double CalculateForValue(double value)
+        {
+            double result = 0;
+
+            for (int i = 0; i < Coeficients.Count; i++)
+            {
+                result += Math.Pow(Coeficients[i] * value, Degree[i]);
+            }
+
+            return result; 
+        }
+
+        public static Polynomial operator +(Polynomial p1, Polynomial p2)
+        {
+            Polynomial p = new Polynomial();
+
+            for (int i = 0; i < p1.Coeficients.Count; i++)
+            {
+                p.Coeficients.Add(p1.Coeficients[i] + p2.Coeficients[i]);
+                p.Degree.Add(p1.Degree[i] + p2.Degree[i]);
+            }
+
+            return p;
+        }
+
+        public static Polynomial operator -(Polynomial p1, Polynomial p2)
+        {
+            Polynomial p = new Polynomial();
+
+            for (int i = 0; i < p1.Coeficients.Count; i++)
+            {
+                p.Coeficients.Add(p1.Coeficients[i] - p2.Coeficients[i]);
+                p.Degree.Add(p1.Degree[i] - p2.Degree[i]);
+            }
+
+            return p;
+        }
+        public static Polynomial operator *(Polynomial p1, Polynomial p2)
+        {
+            Polynomial p = new Polynomial();
+
+            for (int i = 0; i < p1.Coeficients.Count; i++)
+            {
+                p.Coeficients.Add(p1.Coeficients[i] * p2.Coeficients[i]);
+                p.Degree.Add(p1.Degree[i] * p2.Degree[i]);
+            }
+
+            return p;
+        }
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < Degree.Count; i++)
+            {
+                if(Coeficients[i] < 0)
+                {
+                    builder.Append($"{Coeficients[i]}{Variable}^{Degree[i]}");
+                }
+                else
+                    builder.Append($"+{Coeficients[i]}{Variable}^{Degree[i]}");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Polynomial
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<double> coef1 = new List<double>()
+            {
+                1, 2, 5
+            };
+            List<double> degree1 = new List<double>()
+            {
+                1, 1, 2
+            };
+            List<double> coef2 = new List<double>()
+            {
+                2, 4, 7
+            };
+            List<double> degree2 = new List<double>()
+            {
+                1, 2, 1
+            };
+
+            Polynomial polynomial1 = new Polynomial('x', coef1, degree1);
+            Polynomial polynomial2 = new Polynomial('x', coef2, degree2);
+
+            Console.WriteLine(polynomial1);
+            Console.WriteLine(polynomial2);
+
+            Console.WriteLine($"The result witn num for polinomial 1 is {polynomial1.CalculateForValue(2)}");
+
+            Console.WriteLine($"Plus {polynomial1 + polynomial2}");
+            Console.WriteLine($"Minus {polynomial1 - polynomial2}");
+            Console.WriteLine($"Multiple {polynomial1 * polynomial2}");
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
